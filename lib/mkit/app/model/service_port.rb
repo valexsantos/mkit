8716ddd53d96f62ae785dfe13c6ff890a0f9cb31
Vasco Santos <7835679+valexsantos@users.noreply.github.com>
2024-02-24 11:30:43 +0000

    ssl + auth
    * added ssl support - mkit server & proxy config
    * added auth - by app-key
    * auth mandatory
    * mkit server default https

--- lib/mkit/app/model/service_port.rb
+++ lib/mkit/app/model/service_port.rb
@@ -4,7 +4,7 @@ require 'mkit/exceptions'
 class ServicePort < ActiveRecord::Base
   belongs_to :service
 
-  CONFIG_EXPRESSION=/^(.*?):(.*?):(tcp|http):(.*?)$/
+  CONFIG_EXPRESSION=/^(.*?):(.*?):(tcp|http):(.*?)($|:ssl$|:ssl,(.+))$/
 
   def self.create(service:, config:)
     sp = ServicePort.new(service: service, version: service.version)
@@ -18,22 +18,34 @@ class ServicePort < ActiveRecord::Base
   #     # src:dest:tcp|http:round_robin|leastconn
   #     - 5532:5432:tcp:round_robin
   #     - 5532-6000::tcp:round_robin
+  #     # ssl support:
+  #     # src:dest:tcp|http:round_robin|leastconn[:ssl[,<cert.pem>(mkit.pem default)>]]
+  #     - 443:80:tcp:round_robin:ssl # crt file is mkit.pem
+  #     - 443:80:tcp:round_robin:ssl,/etc/pki/foo.pem # crt file full path
   # model:
   #  service_ports:
   #     - external: 5432
   #       internal: 5432
   #       mode: tcp|http
   #       load_bal: round_robin
+  #       ssl: true|false
+  #       crt: full path
   def parse_config(config)
     ports = config.match(CONFIG_EXPRESSION)
     raise MKIt::InvalidPortsConfiguration.new("no match with config expression $#{CONFIG_EXPRESSION}") if ports.nil?
 
+    puts ports
     self.external_port = ports[1]
     self.internal_port = ports[2]
     self.mode = ports[3]
     self.load_bal = ports[4]
+    self.ssl = !ports[5].nil? && ports[5].start_with?(':ssl') ? 'true':'false'
+    self.crt = ports[7].nil? ? MKIt::Utils.proxy_cert : ports[7]
   end
 
+  def ssl
+    super == 'true'
+  end
   def load_balance
     case self.load_bal
     when /^round_robin$/
